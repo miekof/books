dc54d6fb538ac7050f3028011ff3c39c233ea995
Mieko Furuhashi <moc.mieko@okeim.com>
2018-06-11 22:42:50 -0500

    Change ISBN sets and add more search API
    * Based on Vichi's suggestion, update list of ISBN for comparing
    different API's.
    * Add Library of Congress and WorldCat

--- apiCall.sh
+++ apiCall.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # array of ISBN
 
-isbnArray=("9780735211223" "9780385490818" "9780316316132" "9781449474256" "9781449486792" "9781564779984" "9789945894752" "9781628454963" "9781400201655")
-#isbnArray=("9780735211223")
+isbnArray=("9780911207026" "9780199791002" "9780800723453" "9780961444402" "9781592854868" "9781569243138" "9789945894752" "9780307278715" "9781442471818" "9781400201655")
+#isbnArray=("9780911207026")
 
 
 # Open Library
@@ -15,4 +15,16 @@ done
 for item in ${isbnArray[*]}
 do
   curl "https://www.googleapis.com/books/v1/volumes?q=isbn:$item" -o "apiResponse/Google/$item.json"
+done
+
+# WorldCat
+for item in ${isbnArray[*]}
+do
+curl "http://xisbn.worldcat.org/webservices/xid/isbn/$item?method=getMetadata&format=json&fl=*" -o "apiResponse/WorldCat/$item.json"
+done
+
+# Library of Congress
+for item in ${isbnArray[*]}
+do
+curl "http://lx2.loc.gov:210/lcdb?version=1.1&operation=searchRetrieve&query=bath.isbn=$item&startRecord=1&maximumRecords=1&recordSchema=mods" -o "apiResponse/LibraryOfCongress/$item.xml"
 done
